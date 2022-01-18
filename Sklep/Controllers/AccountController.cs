fdca5fe89fe8f62ae6b5abcdf8a592337ad6dd1b
Tomek2699 <tdyrda99@gmail.com>
2022-01-18 13:43:44 +0100

    Layout

---
+++ Sklep/Controllers/AccountController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Sklep.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
